c4ccccc567e21db0b14a60d4711d82a1dbb2b936
Vadyk8577 <todosenkov@gmail.com>
2025-06-16 18:02:33 +0300

    Chapter4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,16 +1,16 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController(DataContext context) : ControllerBase
-{
 
+public class UsersController(DataContext context) : BaseApiController
+{
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -18,8 +18,8 @@ public class UsersController(DataContext context) : ControllerBase
 
         return users;
     }
-
-    [HttpGet("{id}")]
+    [Authorize]
+    [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUsers(int id)
     {
         var user = await context.User.FindAsync(id);
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,24 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader()
             .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
